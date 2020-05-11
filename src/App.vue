36225ae3128020f4067aaaa19698d6d0433d6ed6
envirodatavis <owen.miles@gmail.com>
2020-05-10 22:48:28 -0400

    Finally got the dev envrionment up with Vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,26 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link>&nbsp;|
-      <router-link to="/about">About</router-link>&nbsp;|
-      <router-link to="/hydrogeology">HydroGeology</router-link>
-    </div>
-    <router-view />
-  </div>
+  <v-app>
+    <template>
+      <div id="app">
+        <div id="nav">
+          <v-row justify="center">
+            <router-link to="/">Home</router-link>&nbsp;|
+            <router-link to="/about">About</router-link>&nbsp;|
+            <router-link to="/hydrogeology">HydroGeology</router-link>
+          </v-row>
+        </div>
+        <router-view />
+      </div>
+    </template>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+export default {
+  name: "App",
 
-#nav {
-  padding: 30px;
-}
+  components: {},
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+  data: () => ({}),
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,39 @@
 <template>
   <div class="home">
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+    <v-container fluid>
+      <v-row justify="center">
+        <v-col justify="center" cols="3">
+          <v-card tile outlined>Here's a card</v-card>
+        </v-col>
+        <v-col justify="center" cols="3">
+          <v-card tile outlined>Here's a card</v-card>
+        </v-col>
+        <v-col justify="center" cols="3">
+          <v-card tile outlined>Here's a card</v-card>
+        </v-col>
+        <v-col justify="center" cols="3">
+          <v-card tile outlined>Here's a card</v-card>
+        </v-col>
+        <v-col justify="center" cols="3">
+          <v-card tile outlined>Here's a card</v-card>
+        </v-col>
+        <v-col justify="center" cols="3">
+          <v-card tile outlined>Here's a card</v-card>
+        </v-col>
+      </v-row>
+    </v-container>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+// import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld
-  }
+    // HelloWorld
+  },
 };
 </script>
