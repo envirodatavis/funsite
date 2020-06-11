0b240a1c3ca64dfd796dd211b50457c26461eb18
envirodatavis <owen.miles@gmail.com>
2020-06-11 10:04:15 -0400

    got a good D3 example working! very basic

--- src/components/DThreeExample.vue
+++ src/components/DThreeExample.vue
@@ -1,15 +1,14 @@
 <template>
   <v-row justify="center">
-    <v-card max-width="400px">
+    <v-card>
       <h1>Line Example</h1>
-      <v-slider min="0" max="100" v-model="multiplierSlider"></v-slider>
-      <p>{{ multiplierSlider }}</p>
-      <svg id="dataviz_area" width="330" height="270">
-        <g style="transform: translate(0, 0px)">
+      <v-slider min="1" max="100" v-model="multiplierSlider"></v-slider>
+      <svg id="dataviz_area" width="500" height="500" style="margin: 5px">
+        <g>
           <rect
-            width="330"
-            height="270"
-            style="fill:none; stroke-width:2; stroke:rgb(100, 100, 100);"
+            width="300"
+            height="300"
+            style="fill:rgba(100,100,100,0.1); stroke-width:1; stroke:rgba(100, 100, 100,0.5);"
           />
           <path :d="line" />
         </g>
@@ -26,14 +25,13 @@ export default {
   name: "vue-line-chart",
   data() {
     return {
-      data: [0, 30, 78, 25, 36, 92, 150], //data startsout here
       line: "", //svg pathis initially blank
-      multiplierSlider: 1
+      multiplierSlider: 50
     };
   },
   mounted() {
     this.calculatePath();
-    this.addSvgStuff();
+    this.initializeSvg();
   },
   watch: {
     multiplierSlider: function() {
@@ -42,44 +40,49 @@ export default {
   },
   computed: {
     dataPlot: function() {
-      return this.data.map(x => x * this.multiplierSlider);
+      let xArray = [...Array(100).keys()]; // make an array of 1 to 100
+      return xArray.map(arr => ({
+        x_field: arr / 2.5,
+        y_field: (arr * arr) / this.multiplierSlider / 4
+      }));
+    },
+    scales: function() {
+      const x = d3.scaleLinear().range([0, 300]); // range is pixel values
+      const y = d3.scaleLinear().range([300, 0]);
+      x.domain([0, 30]); // domain is value
+      y.domain([0, 30]);
+      return { x, y };
     }
   },
   methods: {
-    addSvgStuff() {
-      console.log("I FIRED!");
-      var svg = d3.select("#dataviz_area");
-      var xScale = d3
-        .scaleLinear()
-        .domain([0, 100]) //min and max of data
-        .range([0, 400]); //pixel values
-
+    initializeSvg() {
+      const svg = d3.select("#dataviz_area");
+      d3.select("g").attr("transform", "translate(25,25)");
       svg
+        .append("g")
+        .call(d3.axisBottom(this.scales.x))
+        .attr("transform", "translate(25,325)");
+      svg
+        .append("g")
+        .call(d3.axisLeft(this.scales.y))
+        .attr("transform", "translate(25,25)");
+
+      svg // radnomly put a circle at 2,2 to understand margins and things
         .append("circle")
-        .attr("cx", xScale(20))
-        .attr("cy", 100)
-        .attr("r", 40)
-        .style("fill", "blue");
-      svg.call(d3.axisBottom(xScale)).attr("transform", "translate(10,50)");
-    },
-    getScales() {
-      let dataPlot = this.dataPlot;
-      const x = d3.scaleTime().range([0, 300]);
-      const y = d3.scaleLinear().range([210, 0]);
-      d3.axisLeft().scale(x);
-      d3.axisBottom().scale(y);
-      x.domain(d3.extent(dataPlot, (d, i) => i));
-      y.domain([0, 3000]);
-      return { x, y };
+        .attr("cx", this.scales.x(2))
+        .attr("cy", this.scales.y(2))
+        .attr("r", 2)
+        .style("fill", "blue")
+        .attr("transform", "translate(25,25)");
     },
     calculatePath() {
       // function that calculates the path
       let dataPlot = this.dataPlot;
-      const scale = this.getScales();
+      const scale = this.scales;
       const path = d3 //converts the data into the path code
         .line()
-        .x((d, i) => scale.x(i))
-        .y(d => scale.y(d));
+        .x(arr => scale.x(arr.x_field))
+        .y(arr => scale.y(arr.y_field));
       this.line = path(dataPlot);
     }
 
