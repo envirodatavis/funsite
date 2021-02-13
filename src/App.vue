80f9eb95d0b0d7f58e2a115557c459b0acf7c9fd
envirodatavis <owen.miles@gmail.com>
2021-02-13 16:01:44 -0500

    bleh

--- src/App.vue
+++ src/App.vue
@@ -7,8 +7,8 @@
             <router-link to="/">MAFS 1</router-link>
             <span style="padding-left: 5px;padding-right: 5px;">|</span>
             <router-link to="/mafs2">MAFS 2</router-link>
-            <span style="padding-left: 5px;padding-right: 5px;">|</span>
-            <router-link to="/bad-metronome">Bad Metronome</router-link>
+            <!-- <span style="padding-left: 5px;padding-right: 5px;">|</span>
+            <router-link to="/bad-metronome">Bad Metronome</router-link> -->
             <span style="padding-left: 5px;padding-right: 5px;">|</span>
             <router-link to="/about">About</router-link>
             <!-- <span style="padding-left: 5px;padding-right: 5px;">|</span>
