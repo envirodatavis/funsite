0c9995161e03ce67cf1944382ba011de879a90c8
Owen MILES <owen.miles@list.lu>
2021-04-18 12:42:52 +0200

    a POC for showing 3D models online

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,29 @@
 <template>
   <div class="home">
     <v-container fluid class="ma-0 pa-0">
-      <first-mafs-plot />
+      <br />
+      <br />
+      <v-row justify="center" align="center">
+        <v-card height="605" width="505" outlined tile>
+          <v-card-text> 
+            Click and drag to rotate, press 't' to reset the view to top</v-card-text>
+          <x3d height="500px" width="500px">
+            <test />
+          </x3d>
+        </v-card>
+      </v-row>
     </v-container>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import Test from "../components/TestModel.vue";
 
 export default {
   name: "Home",
   components: {
-    FirstMafsPlot: () => import("../components/MarriedAtFirstSightStatic.vue"),
+    Test,
   },
 };
 </script>
