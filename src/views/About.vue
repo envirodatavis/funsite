2e81b63ce96e50a8da78e2083cf30c37c5575e63
Owen MILES <owen.miles@list.lu>
2021-05-17 17:52:50 +0200

    fixed up the router so it is more obvious when on the gallery

--- src/views/About.vue
+++ src/views/About.vue
@@ -5,17 +5,21 @@
         <v-col align="center">
           <v-card flat max-width="500" color="rgba(0,0,0,0)">
             <v-card-text>
-              I have a background in hydrogeology and more
-              recently front-end web development. I have an interest in science
-              and data communication. I am using this site to experiment with tools and
-              share some things, mostly for fun.
+              I have a background in front-end web development and hydrogeology.
+              I have an interest in science and data communication. I am using
+              this site to experiment with tools and share some things, mostly
+              for fun.
               <br />
               <br />
-              This site is mostly written in Vue and D3, with some other cool
-              packages that I will list eventually. I still have many TODOs.
-              But this is for fun. Most content will look good on
-              mobile too, some may not.
+              This site is in Vue, uses a lot of D3, with some other cool
+              packages that I will list eventually. I still have many TODOs. But
+              this is for fun. Most content will look good on mobile too, some
+              may not.
               <br />
+              <br />
+              <a href="https://github.com/Owen-Miles/funsite">
+                Here is the GitHub link for this site
+              </a>
               <br />
               <div class="separator"></div>
               Big thanks to Steven Kalt, who patiently taught me all about the
@@ -25,19 +29,18 @@
               <br />
               <div class="separator">PS:</div>
               My mother and sister are great artists who also have their own
-              sites:
+              sites, see:
               <a href="https://www.torymiles.com">torymiles.com</a>
               and
-              <a href="https://www.lesliemiles.ca">lesliemiles.ca</a>
-              for more great content.
-              <br />
-              <br />
-              <div class="separator">PPS:</div>
+              <a href="https://www.lesliemiles.ca">lesliemiles.ca</a>.
+              <!-- <br />
+              <br /> -->
+              <!-- <div class="separator">PPS:</div>
               Here is a
               <a href="https://www.https://envirodatav.wordpress.com/.ca">
                 janky old wordpress
               </a>
-              site where I included some more map-y art.
+              site where I included some more map-y art. -->
             </v-card-text>
           </v-card>
         </v-col>
